206c82de1fe88eb61d2a64be4078f54516ee4194
AmirMohammadArabi <arabyjavad53@gmail.com>
2024-03-03 14:08:06 +0330

    Homework is done

--- Answers/40230112090/src/main/java/Advanced.java
+++ Answers/40230112090/src/main/java/Advanced.java
@@ -10,7 +10,8 @@ public class Advanced {
 
      */
     public String wordCensor(String sentence, String word, String newWord){
-        return null;
+        String str1 = sentence.replace(word, newWord) ;
+        return str1 ;
     }
 
     /**
@@ -20,7 +21,26 @@ public class Advanced {
      * @return fullName is a normal full name that just the first letter of firstName & lastName is Capitalized (example : Harry Potter)
      */
     public String normalizingName(String firstName, String lastName){
-        return null;
+        int size1 = firstName.length() ;
+        int size2 = lastName.length() ;
+        String str3 ;
+        String str4 ;
+        String str5 ;
+        if (size1 > 1 && size2 > 1) {
+            String str1 =firstName.substring(0,1).toUpperCase() + firstName.substring(1).toLowerCase() ;
+            String str2 = lastName.substring(0,1).toUpperCase() + lastName.substring(1).toLowerCase() ;
+            str3 = str1 + " " + str2 ;
+            return str3 ;
+        } else if (size1 > 1 && size2 <= 1) {
+            String str1 =firstName.substring(0,1).toUpperCase() + firstName.substring(1).toLowerCase() ;
+            str4 = str1 ;
+            return str4 ;
+        } else if (size1 <= 1 && size2 > 1) {
+            String str2 = lastName.substring(0,1).toUpperCase() + lastName.substring(1).toLowerCase() ;
+            str5 = str2 ;
+            return str5 ;
+        }
+        return null ;
     }
 
     /**
@@ -29,7 +49,21 @@ public class Advanced {
      * @return if word contains Consecutive repeated letters, one of the repeated letters should be omitted
      */
     public String doubleChar(String word) {
-        return null;
+        char[] charArray1 = word.toCharArray();
+        int n = word.length();
+        char[] charArray2 = new char[n];
+        int size1 = 0 , size2 = 0 ;
+        for (int j = 0; j < n-1; j++) {
+            if (charArray1[j] != charArray1[j + 1]) {
+                charArray2[size1++] = charArray1[j] ;
+
+            } else if (charArray1[j] == charArray2[j+1]) {
+                size2++ ;
+            }
+        }
+        charArray2[size1++] = charArray1[n-1] ;
+        String result = new String(charArray2 , 0 , size1) ;
+        return result ;
     }
 }
 
--- Answers/40230112090/src/main/java/Warmup.java
+++ Answers/40230112090/src/main/java/Warmup.java
@@ -6,7 +6,16 @@ public class Warmup {
      * @return is the number th word of the sentence
      */
     public String wordFinder(String sentence, int number) {
-        return null;
+        String []str1 = sentence.split("\\s+");
+        int part = str1.length;
+
+        if(number > part)
+        {
+            return " Number = "+number+" is out Of Bound";
+        }
+        else {
+            return str1[number - 1];
+        }
     }
 
     /**
@@ -16,7 +25,24 @@ public class Warmup {
      * @return if searchForEven is true ? return the number of even numbers : return the number of odd numbers
      */
     public int oddEvenCounter(String number, boolean searchForEven) {
-        return -1;
+        char[] charArray = number.toCharArray() ;
+        int part = number.length() ;
+        int result = 0 ;
+        if (searchForEven) {
+            for(int i = 0 ; i < part ; i++){
+                if (charArray[i]%2 == 0) {
+                    result = result + 1;
+                }
+            }
+            return result ;
+        }else{
+            for(int i = 0 ; i < part ; i++){
+                if (charArray[i]%2 != 0) {
+                    result = result + 1 ;
+                }
+            }
+            return result ;
+        }
     }
 
     /**
@@ -25,6 +51,27 @@ public class Warmup {
      * @return The word that is first in alphabet column
      */
     public String firstWord(String wordA, String wordB) {
+        char[] charArray1 = wordA.toCharArray();
+        char[] charArray2 = wordB.toCharArray();
+        int A = wordA.length();
+        int B = wordB.length();
+        if (A >= B) {
+            for (int i = 0; i < B; i++) {
+                if (charArray1[i] < charArray2[i]) {
+                    return wordA;
+                } else if (charArray1[i] > charArray2[i]) {
+                    return wordB;
+                }
+            }
+        } else {
+            for (int i = 0; i < A; i++) {
+                if (charArray1[i] < charArray2[i]) {
+                    return wordA;
+                } else if (charArray1[i] > charArray2[i]) {
+                    return wordB;
+                }
+            }
+        }
         return null;
     }
 }
